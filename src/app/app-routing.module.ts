c9ddf2895485038925dcb1ddbdb723682a149768
hoald <hoald@ocb.com.vn>
2023-09-15 18:11:01 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,87 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainComponent} from '@modules/main/main.component';
+import {BlankComponent} from '@pages/blank/blank.component';
+import {LoginComponent} from '@modules/login/login.component';
+import {ProfileComponent} from '@pages/profile/profile.component';
+import {RegisterComponent} from '@modules/register/register.component';
+import {DashboardComponent} from '@pages/dashboard/dashboard.component';
+import {AuthGuard} from '@guards/auth.guard';
+import {NonAuthGuard} from '@guards/non-auth.guard';
+import {ForgotPasswordComponent} from '@modules/forgot-password/forgot-password.component';
+import {RecoverPasswordComponent} from '@modules/recover-password/recover-password.component';
+import {MainMenuComponent} from '@pages/main-menu/main-menu.component';
+import {SubMenuComponent} from '@pages/main-menu/sub-menu/sub-menu.component';
+import { EmailManagementComponent } from '@pages/email-management/email-management.component';
+import { AccountManagementComponent } from '@pages/account-management/account-management.component';
+import { AdminManagementComponent } from '@pages/admin-management/admin-management.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        children: [
+            {
+                path: 'profile',
+                component: ProfileComponent
+            },
+            {
+                path: 'blank',
+                component: BlankComponent
+            },
+            {
+                path: 'sub-menu-1',
+                component: SubMenuComponent
+            },
+            {
+                path: 'sub-menu-2',
+                component: BlankComponent
+            },
+            {
+                path: '',
+                component: DashboardComponent
+            },
+            {
+                path: 'email-management',
+                component: EmailManagementComponent
+            },
+            {
+                path: 'account-management',
+                component: AccountManagementComponent
+            },
+            {
+                path: 'admin-management',
+                component: AdminManagementComponent
+            },
+        ]
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        canActivate: [NonAuthGuard]
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        canActivate: [NonAuthGuard]
+    },
+    {
+        path: 'forgot-password',
+        component: ForgotPasswordComponent,
+        canActivate: [NonAuthGuard]
+    },
+    {
+        path: 'recover-password',
+        component: RecoverPasswordComponent,
+        canActivate: [NonAuthGuard]
+    },
+    {path: '**', redirectTo: ''}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
